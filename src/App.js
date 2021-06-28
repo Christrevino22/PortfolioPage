84a16ebfcf0d1e324d690e322e5836a4a4323fec
Christopher Trevino <chrisdadef@gmail.com>
2021-06-27 22:19:03 -0500

    finally got started on my portfolio page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import "./App.css";
 import Nav from "./components/Nav";
 
 function App() {
+  // useEffect(() => {
+  //   fetch()
+  //   return () => {
+      
+  //   }
+  // }, [])
   return (
     <div className="App">
       <Nav />
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,25 @@
+import { AppBar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles({
+  appBar: {
+    backgroundColor: "black",
+    boxShadow: "10px 5px 5px 10px red",
+    borderRadius: "50%",
+  },
+  text: {
+    color: "orange",
+  },
+});
+
 export default function Nav() {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Welcome to your navagation</h1>
-    </div>
+    <AppBar position="static" color="secondary" className={classes.appBar}>
+      <Typography variant="h5" color="textPrimary" className={classes.text}>
+        Christopher Trevino
+      </Typography>
+    </AppBar>
   );
 }
