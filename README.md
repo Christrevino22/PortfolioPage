3d6cda5c49c8dc3331cc46a2c413dc26a04a2e41
Christopher Trevino <chrisdadef@gmail.com>
2021-08-08 20:21:49 -0500

    made some drastic changes and my whole idea changed again lol, but all is going well need to continue coding and staying consistent

--- README.md
+++
@@ -1,8 +0,0 @@
-07-28-2021
-Needed to refresh my memory on what i was doing last time i was doing this stuff
-I had my NAV decorated
-08-01-2021
-Gonna restart everything and go ahead and use some material ui and make it a little easier and faster
-Now looking back it appears i already did that
-KEEP BUILDING DON'T LET NOTHING STOP YOU
-YOU WILL BE A SUCCESSFUL SOFTWARE DEVELOPER!!!
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,10 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import { Container, Typography } from "@material-ui/core";
 
 export default function Intro() {
   return (
-    <div>
-      <h2>Let me see if this comes thru to the User Interface!!!</h2>
-    </div>
-  )
+    <Container >
+      <Typography variant='h4'> Hello! My name is Christopher Trevino!</Typography>
+      <br></br>
+      <Typography variant='h5'> I'm currently a Software Developer!!</Typography>
+    </Container>
+  );
 }
